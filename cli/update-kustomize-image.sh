b59d904992fd40a1a707541897da64fdcf27f0ab
kserain@viceri.com.br <kserain@viceri.com.br>
2023-02-08 08:09:45 -0300

    [CLI] Adding repository to cli

--- cli/update-kustomize-image.sh
+++ cli/update-kustomize-image.sh
@@ -7,7 +7,7 @@ ECRREPOSITORY=${3};
 BUILDNUMBER=${4};
 WORKDIR="/home/vsts/work/_temp/${BUILDNUMBER}";
 FULLIMAGENAME="${ECRIMAGEPREFIX}/${ECRREPOSITORY}:${BUILDNUMBER}";
-REPOSITORY="https://uxzjtmutz73oyazhgw7bklghtm3q63pemvq4hkjyqc3gj5rdiira@dev.azure.com/VICERI-DevOps/SalesBox/_git/SalesBox.GitOps";
+REPOSITORY="https://g3o5pftxou53w3gfvqyqg6tndmxexh5jex3r72wk2tggg4pdpsqa@dev.azure.com/kenerrysoftwareengineer/VSSummit-2023/_git/vssummit-gitops";
 
 createDirectoryIfNotExists(){
     if [ ! -d "${WORKDIR}" ]; then
@@ -30,8 +30,8 @@ setNewImageOnKustomize(){
 
 pushChangesToGitOpsRepository(){
     git add ./kustomization.yaml;
-    git config --local user.email "kserain@viceri.com.br";
-    git config --local user.name "kserain";
+    git config --local user.email "kenerry.software.engineer@gmail.com";
+    git config --local user.name "Kenrry Serain";
     git commit -m "Updating Kustomization Image ${ECRIMAGEPREFIX}/${ECRREPOSITORY}:${BUILDNUMBER}";
     git push "${REPOSITORY}" HEAD:main;
 }
